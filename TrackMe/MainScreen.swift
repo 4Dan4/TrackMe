975fbc62ab5286cedca9777e575f2130deda5277
Daniil Pinigin <d.pinigin@ffintech.com>
2024-07-25 16:19:53 +0500

    initial changes

--- TrackMe/MainScreen.swift
+++ TrackMe/MainScreen.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainScreen.swift
 //  TrackMe
 //
 //  Created by Daniil Pinigin on 25.07.2024.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
 
 }
 
